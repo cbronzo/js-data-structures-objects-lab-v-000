891ba077e49f9ef7735c58a61ed48049ebb6ff1e
Catherine Bronzo <cbronzo@mac.com>
2019-03-16 23:59:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Write your solution in this file!
-const driver = {};
+const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
   const newDriver = {}
   newDriver[key] = value
-  return Object.assign(newDriver, driver);
-  
+  return Object.assign(newDriver, driver)
 }
 
 function destructivelyUpdateDriverWithKeyAndValue() {
