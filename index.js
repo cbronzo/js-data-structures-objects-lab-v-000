1580f5bca49001066da6081745d2f32771e226ba
Catherine Bronzo <cbronzo@mac.com>
2019-03-17 00:00:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function deleteFromDriverByKey() {
   
 }
 
-function destructivelyDeleteFromDriverByKey() {
-  
+function destructivelyDeleteFromDriverByKey(driver, key) {
+   return delete driver[key]
 }
